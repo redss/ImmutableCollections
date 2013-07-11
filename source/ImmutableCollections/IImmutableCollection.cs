94dd836441bcac2de69621f5d1564781828fd20c
redss <ryszard.tarajkowski@gmail.com>
2013-07-11 11:51:47 +0200

    Added basic immutable collection and list interfaces.

---
+++ source/ImmutableCollections/IImmutableCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace ImmutableCollections
+{
+    /// <summary>
+    /// Represents an immutable collection of elements.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    public interface IImmutableCollection<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Gets the number of elements in the collection.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Add element to a new collection.
+        /// </summary>
+        /// <param name="item">The object to add.</param>
+        /// <returns>New collection.</returns>
+        IImmutableCollection<T> Add(T item);
+
+        /// <summary>
+        /// Removes the first occurrence of specified element from a new collection.
+        /// </summary>
+        /// <param name="item">The object to remove.</param>
+        /// <returns>New collection.</returns>
+        IImmutableCollection<T> Remove(T item);
+
+        /// <summary>
+        /// Determines whether the collection contains a specific value.
+        /// </summary>
+        /// <param name="item">The object to locate in the colection.</param>
+        /// <returns>True, if item is found in the collection.</returns>
+        bool Contains(T item);
+    }
+}
---
+++ source/ImmutableCollections/IImmutableList.cs
@@ -0,0 +1,52 @@
+﻿namespace ImmutableCollections
+{
+    /// <summary>
+    /// Represents an immutable collection of objects that can be individually accessed by index.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    public interface IImmutableList<T> : IImmutableCollection<T>
+    {
+        /// <summary>
+        /// Gets the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get.</param>
+        /// <returns>The element at specified index.</returns>
+        T this[int index] { get; }
+
+        /// <summary>
+        /// Determines the index of the specified item in the list.
+        /// </summary>
+        /// <param name="item">The object to locate.</param>
+        /// <returns>The index of item if found, otherwise -1.</returns>
+        int IndexOf(T item);
+
+        /// <summary>
+        /// Adds item to at the tail of a new list.
+        /// </summary>
+        /// <param name="item">The object to add.</param>
+        /// <returns>New list.</returns>
+        new IImmutableList<T> Add(T item);
+
+        /// <summary>
+        /// Inserts item at given index of a new list.
+        /// </summary>
+        /// <param name="index">The zero-based index at which item should be inserted.</param>
+        /// <param name="item">The object to insert into the list.</param>
+        /// <returns>New list.</returns>
+        IImmutableList<T> Insert(int index, T item); 
+
+        /// <summary>
+        /// Removes first occurence of specified element from a new list.
+        /// </summary>
+        /// <param name="item">The object to remove.</param>
+        /// <returns>New list.</returns>
+        new IImmutableList<T> Remove(T item);
+
+        /// <summary>
+        /// Removes element at specified index of a new list.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to remove.</param>
+        /// <returns>New list.</returns>
+        IImmutableList<T> RemoveAt(int index);
+    }
+}
