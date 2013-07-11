f487bfe0d981c06de6b8adc5ca4344922adecf76
redss <ryszard.tarajkowski@gmail.com>
2013-07-11 18:36:31 +0200

    Added BaseImmutableList and CopyImmutableList.

---
+++ source/ImmutableCollections/BaseImmutableList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ImmutableCollections
+{
+    /// <summary>
+    /// Base class for immutable lists.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    public abstract class BaseImmutableList<T> : IImmutableList<T>
+    {
+        // IEnumerable
+
+        public abstract IEnumerator<T> GetEnumerator();
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        // IImmutableCollection
+
+        public abstract int Count { get; }
+
+        public abstract bool Contains(T item);
+
+        // IImmutableList
+
+        public abstract IImmutableList<T> Add(T item);
+
+        IImmutableCollection<T> IImmutableCollection<T>.Add(T item)
+        {
+            return Add(item);
+        }
+
+        public abstract IImmutableList<T> Insert(int index, T item);
+
+        public abstract IImmutableList<T> Remove(T item);
+
+        IImmutableCollection<T> IImmutableCollection<T>.Remove(T item)
+        {
+            return Remove(item);
+        }
+
+        public abstract IImmutableList<T> RemoveAt(int index);
+
+        public abstract T this[int index] { get; }
+
+        public abstract int IndexOf(T item);
+    }
+}
---
+++ source/ImmutableCollections/CopyImmutableList.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace ImmutableCollections
+{
+    public class CopyImmutableList<T> : BaseImmutableList<T>
+    {
+        private readonly List<T> _list;
+
+        public CopyImmutableList(List<T> list = null)
+        {
+            _list = list ?? new List<T>();
+        }
+
+        // IEnumerable
+
+        public override IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        // IImmutableCollection
+
+        public override int IndexOf(T item)
+        {
+            return _list.IndexOf(item);
+        }
+
+        public override int Count
+        {
+            get { return _list.Count; }
+        }
+
+        // IImmutableList
+
+        public override IImmutableList<T> Add(T item)
+        {
+            var newList = new List<T>(_list) {item};
+
+            return new CopyImmutableList<T>(newList);
+        }
+
+        public override IImmutableList<T> Insert(int index, T item)
+        {
+            var newList = new List<T>(_list);
+            newList.Insert(index, item);
+
+            return new CopyImmutableList<T>(newList);
+        }
+
+        public override IImmutableList<T> Remove(T item)
+        {
+            var newList = new List<T>(_list);
+            newList.Remove(item);
+
+            return new CopyImmutableList<T>(newList);
+        }
+
+        public override IImmutableList<T> RemoveAt(int index)
+        {
+            var newList = new List<T>(_list);
+            newList.RemoveAt(index);
+
+            return new CopyImmutableList<T>(newList);
+        }
+
+        public override T this[int index]
+        {
+            get { return _list[index]; }
+        }
+
+        public override bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+    }
+}
