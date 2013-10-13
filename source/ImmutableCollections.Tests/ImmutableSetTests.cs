39a3fb783c587f8225b942dc012e667f1dec9043
redss <ryszard.tarajkowski@gmail.com>
2013-10-13 22:47:46 +0200

    ImmutableSetTests and some refactoring.

---
+++ source/ImmutableCollections.Tests/ImmutableSetTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+
+namespace ImmutableCollections.Tests
+{
+    [TestFixture(typeof(ImmutableHashSet<string>))]
+    [TestFixture(typeof(ImmutableSortedSet<string>))]
+    public class ImmutableSetTests<TCollection>
+        where TCollection : IImmutableSet<string>, new()
+    {
+        [Test]
+        public void AddingNull_ThrowsArgumentNullException()
+        {
+            var set = new TCollection();
+            Assert.Throws<ArgumentNullException>(() => set.Add(null));
+        }
+
+        [Test]
+        public void RemovingNull_ThrowsArgumentNullException()
+        {
+            var set = new TCollection();
+            Assert.Throws<ArgumentNullException>(() => set.Remove(null));
+        }
+    }
+}
